ee2854973e3fcf077b521359a1299afcc1359dd8
RajGupta112 <graj31105@gmail.com>
2025-04-17 19:44:54 +0530

    pop-up functionality

---
+++ src/Dashboard/CreateNewShorten.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CreateNewShorten = ({setOpen,refetch}) => {
+  return (
+    <div>
+      new Shorten
+    </div>
+  )
+}
+
+export default CreateNewShorten
--- src/hooks/useQuery.js
+++ src/hooks/useQuery.js
@@ -1,25 +1,25 @@
- import {useQuery} from "@tanstack/react-query"
-import api from "../api/api"
+import { useQuery } from "@tanstack/react-query";
+import api from "../api/api";
 
- 
- export const useFetchTotalClicks =(token,onError)=>{
-  return useQuery("url-totalClick",
-    async ()=>{
-    return await  api.get("/api/urls/myurls",{
-      Headers:{
-        "Content-type":"appplication/json",
-        Accept:"application/json",
-        Authorization:"Bearer"+ token,
-      },
-    })
-  },{
-    select:(data)=>{
-    const sortedData= data.data.sort(
-      (a,b)=> new Date(b.createdDate)- new Date(a.createdDate)
-    );
-    return sortedData;
-    },onError,
-    staleTime:500
+export const useFetchTotalClicks = (token, onError) => {
+  return useQuery({
+    queryKey: ["url-totalClick"],
+    queryFn: async () => {
+      return await api.get("/api/urls/myurls", {
+        headers: {  
+          "Content-Type": "application/json", 
+          Accept: "application/json",
+          Authorization: "Bearer " + token,
+        },
+      });
+    },
+    select: (data) => {
+      const sortedData = data.data.sort(
+        (a, b) => new Date(b.createdDate) - new Date(a.createdDate)
+      );
+      return sortedData;
+    },
+    onError,
+    staleTime: 500,
   });
-
- };
+};
