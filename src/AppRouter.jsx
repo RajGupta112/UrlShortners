216fde66e8207df6e87ad20d0f759fe8b86fd8d6
RajGupta112 <graj31105@gmail.com>
2025-04-20 20:56:15 +0530

    Fix: added fallback to data and fixed ShortenItem import

---
+++ src/AppRouter.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import ShortenUrlPage from './components/ShortenUrlPage';
+
+const AppRouter = () => {
+  return (
+    <BrowserRouter>
+    <Navbar/>
+      <Routes>
+        <Route path='/' element={<LandingPage />} />
+        <Route path='/about' element={<AboutPage />} />
+        <Route path='/register' element={<RegisterPage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/dashboard' element={<DashboardLayout />} />
+      </Routes>
+      <Footer/>
+    </BrowserRouter>
+  )
+}
+
+export default AppRouter;
+
+export  const SubDomainRouter=()=>{
+  return(
+    <Routes>
+        <Route path='/:url' element={<ShortenUrlPage/>} />
+        
+      </Routes>
+  )
+}
+
+
--- src/hooks/useQuery.js
+++ src/hooks/useQuery.js
@@ -3,7 +3,36 @@ import api from "../api/api";
 
 export const useFetchTotalClicks = (token, onError) => {
   return useQuery({
-    queryKey: ["url-totalClick"],
+    queryKey: ["url-totalClicks"],
+    queryFn: async () => {
+      return await api.get("/api/urls/totalClicks?startDate=2024-01-01&endDate=2025-12-31", {
+        headers: {  
+          "Content-Type": "application/json", 
+          Accept: "application/json",
+          Authorization: "Bearer " + token,
+        },
+      });
+    },
+    select: (data) => {
+      const convertToArray = Object.keys(data.data).map((key) => ({
+        clickDate: key,
+        count: data.data[key], // data.data[2024-01-01]
+    }));
+    
+    return convertToArray;
+
+},
+onError,
+staleTime: 5000
+}
+);
+};
+
+
+
+export const useFetchMyShortUrls = (token, onError) => {
+  return useQuery({
+    queryKey: ["my-ShortenUrls"],
     queryFn: async () => {
       return await api.get("/api/urls/myurls", {
         headers: {  
@@ -15,11 +44,12 @@ export const useFetchTotalClicks = (token, onError) => {
     },
     select: (data) => {
       const sortedData = data.data.sort(
-        (a, b) => new Date(b.createdDate) - new Date(a.createdDate)
+          (a, b) => new Date(b.createdDate) - new Date(a.createdDate)
       );
       return sortedData;
-    },
-    onError,
-    staleTime: 500,
-  });
+  },
+  onError,
+  staleTime: 5000
+}
+);
 };
