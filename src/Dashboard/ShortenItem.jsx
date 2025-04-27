6bf653c0dd583370b828e075c8f6b80c72c90dd6
RajGupta112 <graj31105@gmail.com>
2025-04-27 19:59:25 +0530

    changes

--- src/Dashboard/ShortenItem.jsx
+++ src/Dashboard/ShortenItem.jsx
@@ -110,7 +110,7 @@ const ShortenItem = ({ originalUrl, shortUrl, clickCount, createdDate }) => {
                         onCopy={() => setIsCopied(true)}
                         text={fullShortUrl}
                     >
-                        <div className="flex cursor-pointer gap-1 items-center bg-btnColor py-2 font-semibold shadow-md shadow-slate-500 px-6 rounded-md text-white">
+                        <div className="flex cursor-pointer gap-1 items-center bg-btnColor py-2 font-semibold shadow-md shadow-slate-500 px-6 rounded-md text-black">
                             <button>{isCopied ? "Copied" : "Copy"}</button>
                             {isCopied ? <LiaCheckSolid className="text-md" /> : <IoCopy className="text-md" />}
                         </div>
